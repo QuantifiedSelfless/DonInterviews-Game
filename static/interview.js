ba8198850b832547e44e96eadbcce99cfdb38ba2
slsp8752 <slsp8752@colorado.edu>
2016-04-11 21:55:50 -0600

    Fixed cutoff issues

--- static/interview.js
+++ static/interview.js
@@ -1,21 +1,20 @@
 var user_data;
 function preload() {
-    //imgDC = loadImage('static/Yellow-Tree-logo.png');
     bg_image = loadImage('static/paper2.jpg');
     folder = loadImage('static/manilla.png');
     card = loadImage('static/timecard.png');
     coffee_notes = loadImage('static/cofnotes.png');
     //Eventually this should use the URL param to make an AJAX call
     user_data = {
-        name: "Duggy McSwisher",
-        cons: ["said they disliked work 87 times", 
+        name: "Duggy McSwisher Johnson",
+        cons: ["said they disliked work 87 times and one time they went to a store and they stole a whole bunch of stuff!", 
                 "used the word 'late' 150 times",
-                "shares articles about communism",
+                "shares articles about communism and sometimes he farts and junk, and what? what if this garbage was an ACTUAL 10 year old?",
                 "said drunk 265 times"],
-        pros: ["talked about biking 31 times",
+        pros: ["talked about biking 31 times and had a bike when he was a kid, but then he lost it in another shark accident.",
                 "mentioned coffee 129 times",
-                "said they love technology",
-                "shared an article about career achievement"]
+                "said they love technology, and I bet you that technology loves them back! How cute",
+                "shared an article about career achievement, but has never even tried to make an effort! What a joke. My god. AHHHH... AHHHHHHHHHH"]
     };
     handy = loadFont('static/type2.ttf');
 
@@ -28,7 +27,7 @@ function setup() {
 	fill('rgba(0,0,0,0.75)');
 	noStroke();
     image(folder, .15*windowWidth, .01*windowHeight);
-    image(bg_image, .2*windowWidth, .2*windowHeight, .5*windowWidth, .8*windowHeight);
+    image(bg_image, .2*windowWidth, .2*windowHeight, .6*windowWidth, .8*windowHeight);
 	cardWidth = windowWidth/4.6;
 	cardHeight = card.height/(card.width/(cardWidth));
 	image(card, .02*windowWidth, .25*windowHeight, cardWidth, cardHeight);
@@ -39,25 +38,24 @@ function setup() {
     textSize(42);
     text(user_data.name,.25*windowWidth, .27*windowHeight );
     textStyle(ITALIC);
-    textSize(36);
-    text("Concerns:", .25*windowWidth, .35*windowHeight);
+    textSize(32);
+    text("Positive:", .25*windowWidth, .35*windowHeight);
     textSize(26);
     textStyle(NORMAL);
-	var consList = "";
-    for (var i = 0; i < user_data.cons.length; i++){
-        //text("..." + user_data.cons[i], windowWidth*.3, windowHeight*.37 + (windowHeight*.05*i));
-		consList = consList + "..." + user_data.cons[i] + "\n";
-    }
-	text(consList, windowWidth*.3, windowHeight*.37, windowWidth*.5, windowHeight - windowHeight*.37)
-	
-    textSize(32);
-    text("Positive:", .25*windowWidth, .65*windowHeight);
-    textSize(26);
 	var prosList = "";
     for (var i = 0; i < user_data.pros.length; i++){
 		prosList = prosList + "..." + user_data.pros[i] + "\n";
     }
-	text(prosList, windowWidth*.3, windowHeight*.67, windowWidth*.5, windowHeight - windowHeight*.65)
+	text(prosList, windowWidth*.25, windowHeight*.37, windowWidth*.5, windowHeight - windowHeight*.8)
+	
+    textSize(36);
+    text("Concerns:", .25*windowWidth, .65*windowHeight);
+    textSize(26);
+	var consList = "";
+    for (var i = 0; i < user_data.cons.length; i++){
+		consList = consList + "..." + user_data.cons[i] + "\n";
+    }
+	text(consList, windowWidth*.25, windowHeight*.67, windowWidth*.5, windowHeight - windowHeight*.68)
 }
 
 function draw() {
