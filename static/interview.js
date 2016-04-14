da960f5f807f53d9e93757e36c45da775cf65da3
wannabeCitizen <doorsofskirpan@gmail.com>
2016-04-13 20:16:07 -0600

    added jquery and ready to test with live data

--- static/interview.js
+++ static/interview.js
@@ -1,21 +1,39 @@
 var user_data;
+var ready = false;
 function preload() {
     bg_image = loadImage('static/paper2.jpg');
     folder = loadImage('static/manilla.png');
     card = loadImage('static/timecard.png');
     coffee_notes = loadImage('static/cofnotes.png');
     //Eventually this should use the URL param to make an AJAX call
-    user_data = {
-        name: "Duggy McSwisher Johnson",
-        cons: ["said they disliked work 87 times and one time they went to a store and they stole a whole bunch of stuff!", 
-                "used the word 'late' 150 times",
-                "shares articles about communism and sometimes he farts and junk, and what? what if this garbage was an ACTUAL 10 year old?",
-                "said drunk 265 times"],
-        pros: ["talked about biking 31 times and had a bike when he was a kid, but then he lost it in another shark accident.",
-                "mentioned coffee 129 times",
-                "said they love technology, and I bet you that technology loves them back! How cute",
-                "shared an article about career achievement, but has never even tried to make an effort! What a joke. My god. AHHHH... AHHHHHHHHHH"]
-    };
+    // params = getURLParams();
+    var baseurl = 'http://quantifiedselfbackend.local:6060/interview_procesor/interview?';
+    $.ajax({
+            url: baseurl + "userid=b9bef55d-e1c2-418b-979d-62762902ee38",
+            success: function(data) {
+                console.log(data.data);
+                user_data = data.data;
+                ready = true;
+                setup();
+            },
+            error: function(resp) {
+                console.log("didn't work");
+                setTimeout( function () { 
+                    window.location.reload();
+                }, 5000);
+        }
+    });
+    // user_data = {
+    //     name: "Duggy McSwisher Johnson",
+    //     cons: ["said they disliked work 87 times and one time they went to a store and they stole a whole bunch of stuff!", 
+    //             "used the word 'late' 150 times",
+    //             "shares articles about communism and sometimes he farts and junk, and what? what if this garbage was an ACTUAL 10 year old?",
+    //             "said drunk 265 times"],
+    //     pros: ["talked about biking 31 times and had a bike when he was a kid, but then he lost it in another shark accident.",
+    //             "mentioned coffee 129 times",
+    //             "said they love technology, and I bet you that technology loves them back! How cute",
+    //             "shared an article about career achievement, but has never even tried to make an effort! What a joke. My god. AHHHH... AHHHHHHHHHH"]
+    // };
     handy = loadFont('static/type2.ttf');
 
 }
@@ -39,23 +57,28 @@ function setup() {
     text(user_data.name,.25*windowWidth, .27*windowHeight );
     textStyle(ITALIC);
     textSize(32);
-    text("Positive:", .25*windowWidth, .35*windowHeight);
-    textSize(26);
-    textStyle(NORMAL);
-	var prosList = "";
-    for (var i = 0; i < user_data.pros.length; i++){
-		prosList = prosList + "..." + user_data.pros[i] + "\n";
+    if (ready == true) {
+        text("Positive:", .25*windowWidth, .35*windowHeight);
+        textSize(26);
+        textStyle(NORMAL);
+    	var prosList = "";
+        for (var i = 0; i < user_data.pros.length; i++){
+    		prosList = prosList + "..." + user_data.pros[i] + "\n";
+        }
+    	text(prosList, windowWidth*.25, windowHeight*.37, windowWidth*.5, windowHeight - windowHeight*.8)
+    	
+        textSize(36);
+        text("Concerns:", .25*windowWidth, .65*windowHeight);
+        textSize(26);
+    	var consList = "";
+        for (var i = 0; i < user_data.cons.length; i++){
+    		consList = consList + "..." + user_data.cons[i] + "\n";
+        }
+    	text(consList, windowWidth*.25, windowHeight*.67, windowWidth*.5, windowHeight - windowHeight*.68);
+    } else {
+        textSize(36);
+        text("Hold on, getting information for you Don.", windowWidth*.25, windowHeight*.37, windowWidth*.5, windowHeight - windowHeight*.8);
     }
-	text(prosList, windowWidth*.25, windowHeight*.37, windowWidth*.5, windowHeight - windowHeight*.8)
-	
-    textSize(36);
-    text("Concerns:", .25*windowWidth, .65*windowHeight);
-    textSize(26);
-	var consList = "";
-    for (var i = 0; i < user_data.cons.length; i++){
-		consList = consList + "..." + user_data.cons[i] + "\n";
-    }
-	text(consList, windowWidth*.25, windowHeight*.67, windowWidth*.5, windowHeight - windowHeight*.68)
 }
 
 function draw() {
